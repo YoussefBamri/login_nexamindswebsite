35d188cd40d21b76d0cc86361e93e5a633e140f2
asmaayed31 <151970580+asmaayed31@users.noreply.github.com>
2025-01-22 11:38:08 +0000

    chore: adding tailwind (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,25 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
+      <div >
+        <a href="https://vite.dev" target="_blank ">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" >
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      </div> 
+      <h1 className="bg-red-100 text-orange-600">bienvenue </h1>
+      <div className="card bg-orange-600 w-200">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+           click <code> count</code> pour pouvoir compter
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more   
       </p>
     </>
   )
